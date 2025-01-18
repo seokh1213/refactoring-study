3bcd7acb0c5f702a0a7091540fc4dac3da0cd566
홍석현 <seokh1213@gmail.com>
2025-01-18 19:27:40 +0900

    commit: html 렌더링 변경 및 테스트 코드 변경

--- src/main/kotlin/monster/monkeyking/chapter01/statement.kt
+++ src/main/kotlin/monster/monkeyking/chapter01/statement.kt
@@ -2,20 +2,34 @@ package monster.monkeyking.chapter01
 
 fun statement(invoice: Invoice, plays: Plays): String {
     val statementData = createStatementData(plays, invoice)
-    return renderPlainText(statementData)
+    return renderHtml(statementData)
 }
 
-private fun renderPlainText(data: StatementData): String {
-    fun usd(aNumber: Int): String {
-        return "$${aNumber / 100}.00"
-    }
+private fun usd(aNumber: Int): String {
+    return "$${aNumber / 100}.00"
+}
 
-    var result = "청구 내역 (고객명: ${data.customer})\n"
-    for (perf in data.performances) {
-        // 청구 내역을 출력한다.
-        result += "  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n"
-    }
-    result += "총액: ${usd(data.totalAmount)}\n"
-    result += "적립 포인트: ${data.totalVolumeCredits} 점\n"
-    return result
+private fun renderHtml(data: StatementData): String {
+    return StringBuilder().apply {
+        append("<h1>청구 내역 (고객명: ${data.customer})</h1>\n")
+
+        append("<table>\n")
+        append("  <tr>\n")
+        append("      <th>연극</th>\n")
+        append("      <th>좌석수</th>\n")
+        append("      <th>금액</th>\n")
+        append("  </tr>\n")
+        data.performances.forEach { perf ->
+            append("  <tr>\n")
+            append("      <td>${perf.play.name}</td>\n")
+            append("      <td>${perf.audience}석</td>\n")
+            append("      <td>${usd(perf.amount)}</td>\n")
+            append("  </tr>\n")
+        }
+        append("</table>\n")
+
+        append("<p>총액: <em>${usd(data.totalAmount)}</em></p>\n")
+        append("<p>적립 포인트: <em>${data.totalVolumeCredits}</em> 점</p>")
+
+    }.toString()
 }
--- src/test/kotlin/monster/monkeyking/chapter01/StatementTest.kt
+++ src/test/kotlin/monster/monkeyking/chapter01/StatementTest.kt
@@ -25,12 +25,31 @@ class StatementTest {
 
         assertEquals(
             """
-                청구 내역 (고객명: BigCo)
-                  Hamlet: $650.00 (55석)
-                  As You Like It: $580.00 (35석)
-                  Othello: $500.00 (40석)
-                총액: $1730.00
-                적립 포인트: 47 점
+<h1>청구 내역 (고객명: BigCo)</h1>
+<table>
+  <tr>
+      <th>연극</th>
+      <th>좌석수</th>
+      <th>금액</th>
+  </tr>
+  <tr>
+      <td>Hamlet</td>
+      <td>55석</td>
+      <td>$650.00</td>
+  </tr>
+  <tr>
+      <td>As You Like It</td>
+      <td>35석</td>
+      <td>$580.00</td>
+  </tr>
+  <tr>
+      <td>Othello</td>
+      <td>40석</td>
+      <td>$500.00</td>
+  </tr>
+</table>
+<p>총액: <em>$1730.00</em></p>
+<p>적립 포인트: <em>47</em> 점</p>
             """.trimIndent(),
             statement(invoice, plays).trimIndent()
         )
